68047018bbcd287e16ad670f97dcfcd34d2ab721
xJoskiy <pavel5553-20@mail.ru>
2024-05-27 01:29:18 +0300

    Change test file location

--- tests/test_tape.cpp
+++ tests/test_tape.cpp
@@ -3,16 +3,15 @@
 
 #include "Tape.h"
 
+std::string input_file = "test_data/input_tape.txt";
+
 TEST(TestTape, TestGetSize) {
-    std::string input_file = "input_tape.txt";
     Tape tape(input_file,  std::ios::in | std::ios::out);
-    ASSERT_EQ(tape.get_size(), 14);
+    ASSERT_EQ(tape.get_size(), 14); 
 }
 
 TEST(TestTape, TestGet) {
-    std::string input_file = "input_tape.txt";
     Tape tape(input_file,  std::ios::in | std::ios::out);
-
     ASSERT_EQ(tape.get(0), 0);
     ASSERT_EQ(tape.get(5), 5);
     ASSERT_EQ(tape.get(12), 12);
